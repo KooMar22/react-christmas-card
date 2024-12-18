7babb99f14dba12f1e6dfbc8dcdc914dd3d1258e
KooMar <djhracak@gmail.com>
2024-12-18 15:33:25 +0100

    commit - trying to implement music playing for IOS users, too.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import LandingPage from "./pages/LandingPage";
 import ChristmasPage from "./pages/ChristmasPage";
 import Footer from "./components/Footer"
 
 const App = () => {
   const [isChristmasPage, setIsChristmasPage] = useState(false);
+  const audioRef = useRef(null);
 
   const handleButtonClick = () => {
     setIsChristmasPage(true);
+
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
   };
 
   return (
     <div className="app">
+      <audio ref={audioRef} src="/assets/music/Sleigh Ride.mp3" loop />
       {isChristmasPage ? (
         <ChristmasPage />
       ) : (
--- src/pages/ChristmasPage.jsx
+++ src/pages/ChristmasPage.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "../christmaspage.css";
 import Snowflakes from "../components/Snowflakes";
 import Repatica from "../components/Repatica";
@@ -7,17 +6,6 @@ import Grinch from "../components/Grinch";
 import Krampus from "../components/Krampus";
 
 const ChristmasPage = () => {
-  useEffect(() => {
-    const audio = new Audio("/assets/music/Sleigh Ride.mp3");
-    audio.loop = true;
-    audio.play();
-
-    return () => {
-      audio.pause();
-      audio.currentTime = 0;
-    }
-  }, [])
-  
   return (
     <div className="christmas-page">
       <Snowflakes />
