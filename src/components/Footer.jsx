9cffaac63617d74e415bff0a7b9c5bb33ada8524
KooMar <djhracak@gmail.com>
2025-02-17 18:47:43 +0100

    commit - footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,10 +10,10 @@ function Footer() {
       </div>
       
       <div className="contact-icons">
-        <a href="https://github.com/KooMar22?tab=repositories">
+        <a href="https://github.com/KooMar22?tab=repositories" target="_blank" rel="noopener noreferrer">
           <img src={githubLogo} alt="GitHub Logo" className="icon" />
         </a>
-        <a href="https://www.linkedin.com/in/marko-jurić-305964a9">
+        <a href="https://www.linkedin.com/in/marko-jurić-305964a9" target="_blank" rel="noopener noreferrer">
           <img src={linkedinLogo} alt="LinkedIn Logo" className="icon" />
         </a>
       </div>
